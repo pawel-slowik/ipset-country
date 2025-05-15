0e7d6a5f6e0e9e366cea57b9408f37295eae9451
Paweł Słowik <pawel.slowik@zoho.com>
2025-05-15 11:29:22 +0200

    Parametrize tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,32 +1,51 @@
 import ipaddress
 import time
+from typing import List
+import pytest
 from pytest_mock.plugin import MockerFixture
 from ipset import ipset_commands
 
 
-def test_output(mocker: MockerFixture) -> None:
+@pytest.mark.parametrize(
+    "networks,expected_commands",
+    (
+        pytest.param(
+            [
+                ipaddress.IPv4Network("127.0.0.0/24"),
+            ],
+            [
+                "create -exist country-ZZ hash:net",
+                "create country-ZZ.tmp-20290619060010 hash:net",
+                "add country-ZZ.tmp-20290619060010 127.0.0.0/24",
+                "swap country-ZZ country-ZZ.tmp-20290619060010",
+                "destroy country-ZZ.tmp-20290619060010",
+            ],
+            id="single network",
+        ),
+        pytest.param(
+            [
+                ipaddress.IPv4Network("2.0.0.0/24"),
+                ipaddress.IPv4Network("1.0.0.0/24"),
+            ],
+            [
+                "create -exist country-ZZ hash:net",
+                "create country-ZZ.tmp-20290619060010 hash:net",
+                "add country-ZZ.tmp-20290619060010 1.0.0.0/24",
+                "add country-ZZ.tmp-20290619060010 2.0.0.0/24",
+                "swap country-ZZ country-ZZ.tmp-20290619060010",
+                "destroy country-ZZ.tmp-20290619060010",
+            ],
+            id="networks should be sorted",
+        ),
+    ),
+)
+def test_output(
+        networks: List[ipaddress.IPv4Network],
+        expected_commands: List[str],
+        mocker: MockerFixture,
+) -> None:
     mocker.patch("time.gmtime", return_value=time.gmtime(1876543210))
-    expected = [
-        "create -exist country-ZZ hash:net",
-        "create country-ZZ.tmp-20290619060010 hash:net",
-        "add country-ZZ.tmp-20290619060010 127.0.0.0/24",
-        "swap country-ZZ country-ZZ.tmp-20290619060010",
-        "destroy country-ZZ.tmp-20290619060010",
-    ]
 
-    output = list(ipset_commands("ZZ", [ipaddress.IPv4Network("127.0.0.0/24")]))
+    output = list(ipset_commands("ZZ", networks))
 
-    assert output == expected
-
-
-def test_order() -> None:
-    output = list(ipset_commands(
-        "ZZ",
-        [
-            ipaddress.IPv4Network("2.0.0.0/24"),
-            ipaddress.IPv4Network("1.0.0.0/24"),
-        ],
-    ))
-
-    assert output[2].endswith(" 1.0.0.0/24")
-    assert output[3].endswith(" 2.0.0.0/24")
+    assert output == expected_commands
