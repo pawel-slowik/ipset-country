dc87b29e7a6b34dc454fcef691faf55852c73828
Paweł Słowik <pawel.slowik@zoho.com>
2025-05-15 12:02:08 +0200

    Parametrize tests

--- tests/test_compare_networks.py
+++ tests/test_compare_networks.py
@@ -1,57 +1,77 @@
 import ipaddress
-from ipset import compare_networks
+from typing import List
+import pytest
+from ipset import compare_networks, ComparisionResult
 
 
-def test_identical() -> None:
-    comparision = compare_networks(
-        [ipaddress.IPv4Network("1.1.1.0/24")],
-        [ipaddress.IPv4Network("1.1.1.0/24")],
+@pytest.mark.parametrize(
+    "ipdeny_networks,ripestat_networks,expected_result",
+    (
+        pytest.param(
+            [ipaddress.IPv4Network("1.1.1.0/24")],
+            [ipaddress.IPv4Network("1.1.1.0/24")],
+            ComparisionResult(
+                common_networks=[ipaddress.IPv4Network("1.1.1.0/24")],
+                ipdeny_missing=[],
+                ripestat_missing=[],
+                differences_count=0,
+            ),
+            id="identical",
+        ),
+        pytest.param(
+            [ipaddress.IPv4Network("1.1.1.0/24")],
+            [ipaddress.IPv4Network("1.1.1.0/24"), ipaddress.IPv4Network("2.2.2.0/24")],
+            ComparisionResult(
+                common_networks=[ipaddress.IPv4Network("1.1.1.0/24")],
+                ipdeny_missing=[ipaddress.IPv4Network("2.2.2.0/24")],
+                ripestat_missing=[],
+                differences_count=1,
+            ),
+            id="IPdeny missing",
+        ),
+        pytest.param(
+            [ipaddress.IPv4Network("1.1.1.0/24"), ipaddress.IPv4Network("3.3.3.0/24")],
+            [ipaddress.IPv4Network("1.1.1.0/24")],
+            ComparisionResult(
+                common_networks=[ipaddress.IPv4Network("1.1.1.0/24")],
+                ipdeny_missing=[],
+                ripestat_missing=[ipaddress.IPv4Network("3.3.3.0/24")],
+                differences_count=1,
+            ),
+            id="RIPEstat missing",
+        ),
+        pytest.param(
+            [ipaddress.IPv4Network("1.1.1.0/24"), ipaddress.IPv4Network("3.3.3.0/24")],
+            [ipaddress.IPv4Network("1.1.1.0/24"), ipaddress.IPv4Network("2.2.2.0/24")],
+            ComparisionResult(
+                common_networks=[ipaddress.IPv4Network("1.1.1.0/24")],
+                ipdeny_missing=[ipaddress.IPv4Network("2.2.2.0/24")],
+                ripestat_missing=[ipaddress.IPv4Network("3.3.3.0/24")],
+                differences_count=2,
+            ),
+            id="both missing",
+        ),
+        pytest.param(
+            [ipaddress.IPv4Network("3.3.3.0/24")],
+            [ipaddress.IPv4Network("2.2.2.0/24")],
+            ComparisionResult(
+                common_networks=[],
+                ipdeny_missing=[ipaddress.IPv4Network("2.2.2.0/24")],
+                ripestat_missing=[ipaddress.IPv4Network("3.3.3.0/24")],
+                differences_count=2,
+            ),
+            id="disjoint",
+        ),
     )
-    assert list(comparision.common_networks) == [ipaddress.IPv4Network("1.1.1.0/24")]
-    assert not comparision.ipdeny_missing
-    assert not comparision.ripestat_missing
-    assert comparision.differences_count == 0
+)
+def test_comparision(
+        ipdeny_networks: List[ipaddress.IPv4Network],
+        ripestat_networks: List[ipaddress.IPv4Network],
+        expected_result: ComparisionResult,
+) -> None:
+    comparision = compare_networks(ipdeny_networks, ripestat_networks)
 
-
-def test_ipdeny_missing() -> None:
-    comparision = compare_networks(
-        [ipaddress.IPv4Network("1.1.1.0/24")],
-        [ipaddress.IPv4Network("1.1.1.0/24"), ipaddress.IPv4Network("2.2.2.0/24")],
-    )
-    assert list(comparision.common_networks) == [ipaddress.IPv4Network("1.1.1.0/24")]
-    assert list(comparision.ipdeny_missing) == [ipaddress.IPv4Network("2.2.2.0/24")]
-    assert not comparision.ripestat_missing
-    assert comparision.differences_count == 1
-
-
-def test_ripestat_missing() -> None:
-    comparision = compare_networks(
-        [ipaddress.IPv4Network("1.1.1.0/24"), ipaddress.IPv4Network("3.3.3.0/24")],
-        [ipaddress.IPv4Network("1.1.1.0/24")],
-    )
-    assert list(comparision.common_networks) == [ipaddress.IPv4Network("1.1.1.0/24")]
-    assert not comparision.ipdeny_missing
-    assert list(comparision.ripestat_missing) == [ipaddress.IPv4Network("3.3.3.0/24")]
-    assert comparision.differences_count == 1
-
-
-def test_both_missing() -> None:
-    comparision = compare_networks(
-        [ipaddress.IPv4Network("1.1.1.0/24"), ipaddress.IPv4Network("3.3.3.0/24")],
-        [ipaddress.IPv4Network("1.1.1.0/24"), ipaddress.IPv4Network("2.2.2.0/24")],
-    )
-    assert list(comparision.common_networks) == [ipaddress.IPv4Network("1.1.1.0/24")]
-    assert list(comparision.ipdeny_missing) == [ipaddress.IPv4Network("2.2.2.0/24")]
-    assert list(comparision.ripestat_missing) == [ipaddress.IPv4Network("3.3.3.0/24")]
-    assert comparision.differences_count == 2
-
-
-def test_disjoint() -> None:
-    comparision = compare_networks(
-        [ipaddress.IPv4Network("3.3.3.0/24")],
-        [ipaddress.IPv4Network("2.2.2.0/24")],
-    )
-    assert not comparision.common_networks
-    assert list(comparision.ipdeny_missing) == [ipaddress.IPv4Network("2.2.2.0/24")]
-    assert list(comparision.ripestat_missing) == [ipaddress.IPv4Network("3.3.3.0/24")]
-    assert comparision.differences_count == 2
+    assert list(comparision.common_networks) == expected_result.common_networks
+    assert list(comparision.ipdeny_missing) == expected_result.ipdeny_missing
+    assert list(comparision.ripestat_missing) == expected_result.ripestat_missing
+    assert comparision.differences_count == expected_result.differences_count
